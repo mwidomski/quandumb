69d5127acd22591ca168ef0a240c337bfc3deacb
Chris Wozniak <chwoznia@cisco.com>
2017-02-21 22:49:44 -0700

    refactored memslice again

--- mem_share.py
+++ mem_share.py
@@ -1,32 +1,41 @@
-def set_chunk(self, memslice):
-    self.buf[memshare.offset:] = memslice.data
+MEMSLICE_HEADER = 16
 
-def make_slices(self, buf, slice_count):
-    slice_size = self.buf.size() / slice_count
+def set_chunk(self, buf, memslice):
+    buf[memslice.head["offset"]:] = memslice.data
 
+def make_slices(self, buf, static, slice_count):
+    slice_size = buf.len / slice_count
     slices = []
     for i in range(slice_count):
-        offset = data_start + (slice_size * i)
-        head  = {"head": SLICE_HEAD_SIZE,
-                 "scene": self.scene_size,
-                 "data": slice_size,
-                 "offset": offset}
-        scene = self.buf[offset:(offset + self.scene_size)]
-        data  = self.buf[offset:(offset + slice_size)]
-        slices.append(MemSlice(head, scene, data))
-
+        buf_offset = (slice_size * i)
+        head  = {"head": MEMSLICE_HEADER,
+                 "static": static.len
+                 "data": buf.len,
+                 "offset": buf_offset}
+        data = buf[offset:(offset + slice_size)]
+        slices.append(MemSlice(head, static, data))
     return slices
 
 class MemSlice:
 
-    def __init__(self, head data):
-        self.head = head
-        self.data = datastream
+    def __init__(self, head , static = None, data = None):
+        if data is not None:
+            self.head = head
+            self.static = static
+            self.data = datastream
+        else:
+            from_bytestream(head);
+
+    
+    def from_bytestream():
+        #TODO
+        return None
 
     def to_bytestream:
         byte = bytearray()
-        headers = ["head", "offset", "data"]
+        headers = ["head", "static", "offset", "data"]
         for head in headers:
             byte = byte.join(bytearray(self.head[head]))
+        byte = byte.join(bytearray(self.static))
         byte = byte.join(bytearray(self.data))
         return byte
