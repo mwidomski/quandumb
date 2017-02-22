9d9d7740906c66a95b73f9e43736bf03e097e7c8
Heroist Hero <firestinger_1@hotmail.com>
2017-02-21 23:02:32 -0700

    Reimplemented ClusterManager as a library
    -removed redundant imports in ClusterManager
    -renamed createConnectors() to create_connectors() in ClusterManager
    -removed main() in ClusterManager
    -removed uses of memShare in ClusterManager
    -implemented __init__ in ClusterManager

--- ClusterManager.py
+++ ClusterManager.py
@@ -1,13 +1,9 @@
-import threading
-import socket
-import select
 import Connector
-import mem_share as MemShare
 
 class ClusterManager:
     filename = "config.config"
 
-    def createConnectors(self):
+    def create_connectors(self):
         configfile = open(self.filename, "r")
         connectorList = []
         
@@ -19,19 +15,6 @@ class ClusterManager:
             connectorList.append(Connector(sliceIp,int(slicePort)))
         return connectorList
             
-    def main(self):
+    def __init__(self):
         #read config file and parse slice's ip and port
-        connectorList = self.createConnectors()
-
-        #set up the shared memory
-        memShare = MemShare()
-
-        #read from memshares to connectors
-        slices = memShare.make_slices(len(connectorList))
-
-        for i in range(len(connectorList)):
-            connectorList[i].send_slice(slices[i])
-
-        #read from connectors to memshare
-        for i in range(len(connectorList)):
-            memShare.set_chunk(connectorList[i].get_slice())
+        connectorList = self.create_connectors()
