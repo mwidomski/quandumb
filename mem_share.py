0cecdc58c419e5db45865116f9a65c4e1b38efcc
Chris Wozniak <chwoznia@cisco.com>
2017-02-21 22:19:14 -0700

    added MemSlice

--- mem_share.py
+++ mem_share.py
@@ -7,12 +7,27 @@ def make_slices(self, buf, slice_count):
     slices = []
     for i in range(slice_count):
         offset = data_start + (slice_size * i)
-        head  = {"head_size": SLICE_HEAD_SIZE,
-                 "scene_size": self.scene_size,
-                 "data_size": slice_size,
+        head  = {"head": SLICE_HEAD_SIZE,
+                 "scene": self.scene_size,
+                 "data": slice_size,
                  "offset": offset}
         scene = self.buf[offset:(offset + self.scene_size)]
         data  = self.buf[offset:(offset + slice_size)]
         slices.append(MemSlice(head, scene, data))
 
     return slices
+
+class MemSlice:
+
+    def __init__(self, head, datastream):
+        self.head = head
+        self.scene = scene
+        self.data = datastream
+
+    def to_bytestream:
+        byte = bytearray()
+        headers = ["head", "scene", "data", "offset"]
+        for head in headers:
+            byte = byte.join(bytearray(self.head[head]))
+        byte = byte.join(bytearray(self.data))
+        return byte
